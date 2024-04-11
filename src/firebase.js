16cfd2438f57134aa46ed10f8cd81a6105fbadfb
chelseaoh <ChelseaO@csu.fullerton.edu>
2024-04-11 13:52:57 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC0xspAvSbvo0tcpGSU3_fiHxn-IOlRY-A",
-  authDomain: "cabelle-6c998.firebaseapp.com",
-  projectId: "cabelle-6c998",
-  storageBucket: "cabelle-6c998.appspot.com",
-  messagingSenderId: "825339706675",
-  appId: "1:825339706675:web:d0b09b7de506d9cfccd246",
-  measurementId: "G-41SJDXHNZ1"
+  apiKey: "AIzaSyD2w9wR2W7hHjm1vojOmMO7cX-_93QtZWI",
+  authDomain: "cabelle-a0a69.firebaseapp.com",
+  projectId: "cabelle-a0a69",
+  storageBucket: "cabelle-a0a69.appspot.com",
+  messagingSenderId: "735995960774",
+  appId: "1:735995960774:web:f0fa81b21871c044b7a6c7",
+  measurementId: "G-MF101YKX7H"
 };
 
 // Initialize Firebase
