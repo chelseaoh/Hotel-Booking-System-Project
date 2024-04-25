c3048468e06de8c40e834ff50a69c6c64cbc46bf
pachmi <123521461+pachmi@users.noreply.github.com>
2024-04-25 14:15:04 -0700

    updates login not completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+const registerlink = document.querySelectorAll('.register-link');
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active')
+};
