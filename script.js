7aa22e46008a84e7e45669b2df69da670ed96b95
pachmi <123521461+pachmi@users.noreply.github.com>
2024-04-30 14:40:56 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const wrapper = document.querySelector('.wrapper');
-const registerlink = document.querySelectorAll('.register-link');
-
-registerLink.onclick = () => {
-    wrapper.classList.add('active')
-};
