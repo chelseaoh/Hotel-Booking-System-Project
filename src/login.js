3029a54e38b1ec4bee112fbe5a1a0c586790d6be
Carlos <112414767+Carlos00101@users.noreply.github.com>
2024-05-06 16:10:54 -0700

    Create login.js

---
+++ src/login.js
@@ -0,0 +1,166 @@
+import './App.css';
+import React, { useState } from 'react';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2w9wR2W7hHjm1vojOmMO7cX-_93QtZWI",
+  authDomain: "cabelle-a0a69.firebaseapp.com",
+  databaseURL: "https://cabelle-a0a69-default-rtdb.firebaseio.com",
+  projectId: "cabelle-a0a69",
+  storageBucket: "cabelle-a0a69.appspot.com",
+  messagingSenderId: "735995960774",
+  appId: "1:735995960774:web:f0fa81b21871c044b7a6c7",
+  measurementId: "G-MF101YKX7H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {  
+const [hide, sethide] = useState(true);
+const [show, setshow] = useState(true);
+const validateForm = (event) => {
+  event.preventDefault();
+  const input1 = document.querySelector("#input1");
+  const input2 = document.querySelector("#input2");
+  const status = document.querySelector(".status");
+
+  let validation = "Please fill-in the above fields";
+  let emailVal = "Email validation error must inclued @ .com TryAgain!";
+  let passVal = "Passowrd char must not be less than 8";
+  if(!input1.value) {
+    console.warn("validation error");
+    status.classList.add("active");
+    status.innerHTML = `${validation}`;
+  }else{
+    emailValidate();
+  }
+  function emailValidate() { 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!input1.value.match(pattern)){
+      console.warn("pattern failed")
+      status.classList.add("active");
+      status.innerHTML = `${emailVal}`;
+    }else{
+      console.log("input1 validated");
+      status.classList.remove("active");
+      input1.classList.add("valid");
+    }
+   }
+   if(!input2.value){
+      console.warn("validation error");
+      status.classList.add("active");
+      status.innerHTML = `${validation}`;
+   }else{
+     passwordValidate();
+   }
+   function passwordValidate() {
+     if(input2.value.length < 8){
+       console.warn("Passowrd char must not be less than 8");
+       status.classList.add("active");
+      status.innerHTML = `${passVal}`;
+     }else{
+      console.log("input2 validated");
+      status.classList.remove("active");
+      input2.classList.add("valid");
+     }
+   }
+   if(input1.classList.contains("valid") && input2.classList.contains("valid")){
+     console.log('submitted');
+     setTimeout(() => {
+      //  window.location.href = 'AirbnbHome';
+      let container = document.querySelector(".container");
+      let wrapper = document.querySelector(".wrapper");
+      sethide(!hide + wrapper.classList.add("hide"));
+      setshow(!show + container.classList.add("show"));
+
+       
+     }, 1000);
+   }
+  }
+  const handleMail = () => {
+    const input1 = document.querySelector("#input1");
+    const status = document.querySelector(".status");
+    let validation = "Please fill-in the above fields";
+    let emailVal = "Email validation error must inclued @ .com TryAgain!";
+  if(!input1.value) {
+    console.warn("validation error");
+    status.classList.add("active");
+    status.innerHTML = `${validation}`;
+  }else{
+    emailValidate();
+  }
+  function emailValidate() { 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!input1.value.match(pattern)){
+      console.warn("pattern failed")
+      status.classList.add("active");
+      status.innerHTML = `${emailVal}`;
+    }else{
+      console.log("input1 validated");
+      status.classList.remove("active");
+    }
+   }
+  }
+  const handlePassword  = () => {
+    const input2 = document.querySelector("#input2");
+    const status = document.querySelector(".status");
+  
+    let validation = "Please fill-in the above fields";
+    let passVal = "Passowrd char must not be less than 8";
+     if(!input2.value){
+        console.warn("validation error");
+        status.classList.add("active");
+        status.innerHTML = `${validation}`;
+     }else{
+       passwordValidate();
+     }
+     function passwordValidate() {
+       if(input2.value.length < 8){
+         console.warn("Passoword char must not be less than 8");
+         status.classList.add("active");
+        status.innerHTML = `${passVal}`;
+       }else{
+        console.log("input2 validated");
+        status.classList.remove("active");
+       }
+     }
+  }
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="content">
+          <div className="logo">
+          </div>
+          <div className="c1">
+            <span>Become an member of Cabelle today</span>
+          </div>
+          <div className="form">
+            <div className="status"></div>
+            <form action="#" onSubmit={validateForm}>
+              <div className="eInput">
+                <input id='input1' type="text" placeholder='Enter Email Address' onKeyUp={handleMail}/>
+              </div>
+              <div className="eInput">
+                <input id='input2' type="password" placeholder='Enter Password' onKeyUp={handlePassword}/>
+              </div>
+              <div className="submit">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
